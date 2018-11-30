ab6c0b12e7b71e9fb3b83f8e9586c2631c58d4ca
scott.bishop <scott.bishop@workday.com>
2018-11-29 22:02:57 -0800

    Fix motion sensor, red = power, white = ground, black = gpio

--- things/src/main/java/za/co/riggaroo/motioncamera/MotionSensor.kt
+++ things/src/main/java/za/co/riggaroo/motioncamera/MotionSensor.kt
@@ -3,7 +3,9 @@ package za.co.riggaroo.motioncamera
 import android.arch.lifecycle.Lifecycle
 import android.arch.lifecycle.LifecycleObserver
 import android.arch.lifecycle.OnLifecycleEvent
+import android.util.Log
 import com.google.android.things.pio.Gpio
+import com.google.android.things.pio.GpioCallback
 import com.google.android.things.pio.PeripheralManager
 
 class MotionSensor(private val motionListener: MotionListener,
@@ -13,26 +15,41 @@ class MotionSensor(private val motionListener: MotionListener,
 
     @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
     fun start() {
+        val portList: List<String> = PeripheralManager.getInstance().gpioList
+        if (portList.isEmpty()) {
+            Log.v("MotionSensor", "No GPIO port available on this device.")
+        } else {
+            Log.v("MotionSensor", "List of available ports: $portList")
+        }
         //Receive data from the sensor - DIRECTION_IN
         motionSensorGpioPin.setDirection(Gpio.DIRECTION_IN)
         //High voltage means movement has been detected
         motionSensorGpioPin.setActiveType(Gpio.ACTIVE_HIGH)
         //The trigger we want to receive both low and high triggers so EDGE_BOTH
-        motionSensorGpioPin.setEdgeTriggerType(Gpio.EDGE_BOTH)
-        motionSensorGpioPin.registerGpioCallback { gpio ->
-            gpio?.let {
-                if (gpio.value) {
-                    motionListener.onMotionDetected()
-                } else {
-                    motionListener.onMotionStopped()
-                }
+        motionSensorGpioPin.setEdgeTriggerType(Gpio.EDGE_RISING)
+        motionSensorGpioPin.registerGpioCallback(gpioCallback)
+    }
+
+    private val gpioCallback = object : GpioCallback {
+        override fun onGpioEdge(gpio: Gpio): Boolean {
+            if (gpio.value) {
+                motionListener.onMotionDetected()
+            } else {
+                motionListener.onMotionStopped()
             }
-            true
+
+            // Continue listening for more interrupts
+            return true
+        }
+
+        override fun onGpioError(gpio: Gpio, error: Int) {
+            Log.w("MotionSensor", "$gpio: Error event $error")
         }
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
     fun stop() {
+        motionSensorGpioPin.unregisterGpioCallback(gpioCallback)
         motionSensorGpioPin.close()
     }
 
